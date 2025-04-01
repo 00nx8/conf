5a3c5b69021a6d86770cf3390a3583930d881aae
00nx6 <lszabo958@gmail.com>
2025-04-01 22:06:44 +0200

    some more configs and updated mv.sh

---
+++ mv.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+sudo pacman -S i3blocks i3status feh i3bar picom i3lock xss-lock nm-applet dmenu playerctl maim xclip i3-gaps xfce4-terminal
+
+mkdir -pv \
+    ~/Documents/system-bg/ \
+    ~/.config/scripts/ \
+    ~/.config/i3/ \
+    ~/.config/i3blocks/ \
+    ~/.config/i3status/ \
+    ~/.config/xfce4/xfconf/xfce-perchannel-xml
+
+cp -v ./xfce4/* ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+cp -v ./wallpaper.png ~/Documents/system-bg/
+cp -v ./lock ~/.config/scripts/
+cp -v ./config ~/.config/i3/
+cp -v ./i3blocks.conf ~/.config/i3blocks/
+cp -v ./i3status.conf ~/.config/i3status/
+
+chmod -v a+x ~/.config/scripts/lock
+
+if [ -n "$DISPLAY" ]; then
+    i3-msg reload
+    i3-msg restart
+fi
---
+++ undo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mv ~/.config/i3/config ./
+
+mv ~/.config/i3blocks/i3blocks.conf ./
+
+mv ~/.config/i3status/i3status.conf ./
